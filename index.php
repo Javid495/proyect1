0ca3796d533b378f89473766d88846d154347545
Javid495 <125301562+Javid495@users.noreply.github.com>
2023-06-13 09:30:57 -0500

    .

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php 
+        require_once 'app.php';
+        include_once __DIR__.'/Templates/header.php';
+?>
+<main>
+
+</main>
+<?php 
+        include_once __DIR__.'/view/Country/regcountry.php';
+?>
+<?php 
+        include_once __DIR__.'/view/Country/selectcountry.php';
+?>
+<?php 
+        include_once __DIR__.'/view/Country/lstpaises.php';
+?>
+<?php 
+        include_once __DIR__.'/view/Country/lstcountry.php';
+?>
+<?php 
+        include_once __DIR__.'/Templates/footer.php';
+?>
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'desarrollo/inventario-php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'accd340aa4ca01851c2c6eed10a61e2042b8670d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'desarrollo/inventario-php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'accd340aa4ca01851c2c6eed10a61e2042b8670d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
